c1b035c84ff190be326217e6695d391f480dc5ff
Hyung1Jung <hyungiljung94@gmail.com>
2020-10-18 00:59:10 +0900

    stream api 기초

--- src/ex1/stream/Stream_Api_Test.java
+++ src/ex1/stream/Stream_Api_Test.java
@@ -7,15 +7,15 @@ public class Stream_Api_Test {
         int[] array = {2, 4, 6};
 
         boolean result = Arrays.stream(array)
-                .allMatch(a -> a%2 == 0);
-        System.out.println("2의 배수? " + result);
+                .allMatch(a -> a * 2 < 20);
+        System.out.println("2의 배수들이 20보다 작나? " + result);
 
         result = Arrays.stream(array)
                 .anyMatch(a -> a%3 == 0);
         System.out.println("3의 배수가 하나라도 있나? " + result);
 
         result = Arrays.stream(array)
-                .noneMatch(a -> a%3 == 0);
-        System.out.println("3의 배수가 없나? " + result);
+                .noneMatch(a -> a%6 == 0);
+        System.out.println("6의 배수가 없나? " + result);
     }
 }
