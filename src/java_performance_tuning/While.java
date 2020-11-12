057f3afc6012389d1ced76d50a62fc9815c33822
Hyung1Jung <junghyungile@gmail.com>
2020-11-12 23:27:01 +0900

    자바 성능튜닝 while

---
+++ src/java_performance_tuning/While.java
@@ -0,0 +1,25 @@
+package java_performance_tuning;
+
+import java.util.List;
+
+public class While {
+
+//    public void example(List<String> words) {
+//        boolean flag = true;
+//        int index = 0;
+//        while (flag) {
+//            if(words.get(index++).equals("book")) {
+//                flag = false;
+//            }
+//        }
+//    }
+
+//    public void example(List<String> words) {
+//        words.stream()
+//                .forEach(word -> System.out.println(word));
+//    }
+//
+//    public void example(List<String> words) {
+//        words.forEach(word -> System.out.println(word));
+//    }
+}
