a669b1907ba0cd933fa9f7ea1ce333f424a6e97a
Hyung1Jung <junghyungile@gmail.com>
2020-11-24 20:50:50 +0900

    FileWriter

---
+++ src/java8/stream/wirter/FileWriterTest.java
@@ -0,0 +1,20 @@
+package java8.stream.wirter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter fw = new FileWriter("writer.txt");
+        fw.write('A');
+        char[] buf = {'B','C','D','E','F'};
+
+        fw.write(buf);
+        fw.write("안녕하세요");
+        fw.write(buf,2,2);
+        fw.close();
+
+        System.out.println("end");
+    }
+}
