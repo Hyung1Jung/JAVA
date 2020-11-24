21aeaf0532be222c925bbfb24f06fd94a91146cf
Hyung1Jung <junghyungile@gmail.com>
2020-11-24 19:46:17 +0900

    FileInputStream & FileInputStream

---
+++ src/java8/stream/outputstream/FileOutputTest2.java
@@ -0,0 +1,31 @@
+package java8.stream.outputstream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputTest2 {
+    public static void main(String[] args) {
+
+        byte[] bs = new byte[26];
+        byte data = 65;
+
+        for(int i = 0; i <bs.length; i++) {
+            bs[i] = data;
+            data++;
+        }
+
+        try(FileOutputStream fos = new FileOutputStream("alpha.txt", true);
+            FileInputStream fis = new FileInputStream("alpha.txt")) {
+
+            fos.write(bs);
+            int ch;
+            while((ch = fis.read()) != -1) {  // 하나씩 읽음
+                System.out.print((char)ch);
+            }
+
+        }catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
